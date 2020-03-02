41d36532d322b66065cc26cdb7c3f7218770f852
Mond <edmond.delmundo@gmail.com>
2020-03-02 22:06:52 +0800

    changed from hs256 to rs256

--- api/middleware.js
+++ api/middleware.js
@@ -1,7 +1,10 @@
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 require('dotenv').config();
 //const secret = 'mysecretsshhh';
 const secret = process.env.SECRET;
+
+const publicKey = fs.readFileSync('./public.key', 'utf-8');
 
 const withAuth = function(req, res, next) {
   const token = 
@@ -13,11 +16,15 @@ const withAuth = function(req, res, next) {
   if (!token) {
     res.status(401).send('Unauthorized: No token provided');
   } else {
-    jwt.verify(token, secret, function(err, decoded) {
+    jwt.verify(token, publicKey, {
+      maxAge: '1h',
+      algorithms: 'RS256'
+    }, function(err, decoded) {
       if (err) {
         res.status(401).send('Unauthorized: Invalid token');
       } else {
         req.username = decoded.username;
+        req.role = decoded.role;
         next();
       }
     });
