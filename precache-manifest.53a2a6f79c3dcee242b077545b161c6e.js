5045a95a629b1a9ff761d74be9324092d251c234
edmond.delmundo <edmond.delmundo@wolterskluwer.com>
2020-09-25 18:41:09 +0800

    Updates

---
+++ precache-manifest.53a2a6f79c3dcee242b077545b161c6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7031724978b7f445358c93613b5e95e0",
+    "url": "/competition-judging-system/index.html"
+  },
+  {
+    "revision": "1065fd4db2cd70f41ae6",
+    "url": "/competition-judging-system/static/css/2.4424f763.chunk.css"
+  },
+  {
+    "revision": "2f341d0f6024f8b81fd7",
+    "url": "/competition-judging-system/static/css/main.f3c557cd.chunk.css"
+  },
+  {
+    "revision": "1065fd4db2cd70f41ae6",
+    "url": "/competition-judging-system/static/js/2.2cc3fdf9.chunk.js"
+  },
+  {
+    "revision": "7351bde4ce2898732dc7e616c6dd7463",
+    "url": "/competition-judging-system/static/js/2.2cc3fdf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f341d0f6024f8b81fd7",
+    "url": "/competition-judging-system/static/js/main.3015d2b8.chunk.js"
+  },
+  {
+    "revision": "2bb9a78bf94f58ef12c9",
+    "url": "/competition-judging-system/static/js/runtime-main.944ec241.js"
+  }
+]);
