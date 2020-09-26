1f56b4b3edbd727beecdc285e027c8cb87c6ba85
edmond.delmundo <edmond.delmundo@wolterskluwer.com>
2020-09-26 10:27:23 +0800

    Test

--- ui/src/App.js
+++ ui/src/App.js
@@ -41,9 +41,126 @@ function App(props) {
     judgeHasAuthenticated(false);
     props.history.push("/login"); //will redirect the user to login page after logging out
   }
+assName="App contain  //testing  //testing
 
-    <div className="App contain  //testing
 er">
+      <Notifications />
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <NavLink className="navbar-brand" to="/">
+          Adjudicator
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        {isAuthenticated ? (
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <NavLink className="Nav_Link" to="/events">
+                Events
+              </NavLink>
+              <NavLink className="Nav_Link" to="/competitions">
+                Competitions
+              </NavLink>
+              <NavLink className="Nav_Link" to="/judges">
+                Judges
+              </NavLink>
+              <NavLink className="Nav_Link" to="/churches">
+                Churches
+              </NavLink>
+            </Nav>
+            <Nav className="ml-auto">
+              <NavLink className="Nav_Link Log" to="" onClick={handleLogout}>
+                Logout
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        ) : isAuthenticatedJudge ? (
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <NavLink className="Nav_Link" to="/scoresheet">
+                Scoresheet
+              </NavLink>
+            </Nav>
+            <Nav className="ml-auto">
+              <NavLink className="Nav_Link Log" to="" onClick={handleLogout}>
+                Logout
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        ) : (
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <NavLink className="Nav_Link Log" to="/login">
+                Login
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        )}
+      </Navbar>
+
+      <Routes
+        appProps={{
+          isAuthenticated,
+          userHasAuthenticated,
+          isAuthenticatedJudge,
+          judgeHasAuthenticated,
+          decodedUser,
+          setDecodedUser,
+          decodedJudge,
+          setDecodedJudge,
+        }}
+      />
+    </div>
+  );
+}
+
+//export default withRouter(App);
+export default withRouter(App);
+import React, { useState, useEffect } from "react";
+import { NavLink, withRouter } from "react-router-dom";
+import "./App.css";
+import "./DefaultStyles.css";
+import Routes from "./Routes";
+import Auth from "./Auth";
+import { Nav, Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Notifications from "./components/notifications/Notification";
+
+function App(props) {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [decodedUser, setDecodedUser] = useState({});
+  const [decodedJudge, setDecodedJudge] = useState({});
+  const [isAuthenticatedJudge, judgeHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      if (await Auth.isUserAuthenticated()) {
+        userHasAuthenticated(true);
+      }
+      if (await Auth.isJudgeAuthenticated()) {
+        judgeHasAuthenticated(true);
+      }
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e);
+      }
+    }
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.deauthenticateUser();
+    userHasAuthenticated(false);
+    judgeHasAuthenticated(false);
+    props.history.push("/login"); //will redirect the user to login page after logging out
+  }
+
+  return (
+    <div className="App container">
       <Notifications />
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <NavLink className="navbar-brand" to="/">
